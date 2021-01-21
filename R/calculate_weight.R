2102fb54bf57fb8636de59dfbf82dfbdc1949d76
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-21 15:55:38 +0000

    Added documentation for calculate_weight

--- R/calculate_weight.R
+++ R/calculate_weight.R
@@ -1,3 +1,12 @@
+#' Calculate the mass of a protein
+#'
+#' @param protein A protein structure created by load_protein
+#'
+#' @return The mass of the protein
+#' @export
+#'
+#' @examples
+#' calculate_weight(load_protein("MAGFEKKPW"))
 calculate_weight <- function(protein) {
   protein %>%
     dplyr::left_join(weights,by=c("seq"="One")) %>%
