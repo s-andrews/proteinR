59df137d256eaff77bb4fc4b1b5a12e07b2e081d
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-01-21 15:31:27 +0000

    Validated load_protein input

--- R/load_protein.R
+++ R/load_protein.R
@@ -1,6 +1,14 @@
 load_protein <- function(sequence) {
 
+  assertthat::assert_that(assertthat::is.string(sequence))
+
+  toupper(sequence) -> sequence
+
   stringr::str_split(sequence,"")[[1]] -> seqvector
+
+  stringr::str_split("ARNDCEQGHILKMFPSTWYV","")[[1]] -> valid_letters
+
+  assertthat::assert_that(all(seqvector %in% valid_letters), msg = "Non-amino-acid letters found in sequence")
 
   tibble::tibble(
     pos=1:length(seqvector),
